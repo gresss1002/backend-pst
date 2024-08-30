7b0555d585f8bcf206b1b97b652c7114bea4c7b7
gresss1002 <102232893+gresss1002@users.noreply.github.com>
2024-08-30 20:23:56 +0700

    new

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,36 +4,36 @@ interface IUser extends Document {
   googleId: string;
   email: string;
   name: string;
-  role: "user" | "admin" | "konsultan";
+  role: "konsumen" | "admin" | "konsultan";
   createdAt: Date;
   gender: "Perempuan" | "Laki-laki";
-  birthdate: string;
-  phoneNumber: string;
-  addressProv: string;
-  addressKab: string;
+  birthDate: string;
+  telepon: string;
+  province: string;
+  regency: string;
   work: "PNS" | "Swasta" | "Lainnya";
   education: "SLTA" | "D1/D2/D3" | "D4/S1" | "S2" | "S3";
   position: string;
-  field: string;
-  days: [string];
+  field: [string];
+  available: [string];
 }
 
 const UserSchema: Schema = new Schema({
   googleId: { type: String, required: true },
   email: { type: String, required: true },
   name: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin', 'konsultan'], default: 'user' },
+  role: { type: String, enum: ['konsumen', 'admin', 'konsultan'], default: 'konsumen' },
   createdAt: { type: Date, default: Date.now },
   gender: { type: String, enum: ['Perempuan', 'Laki-laki'] },
-  birthdate: { type: String, default: '' },
-  phoneNumber: { type: String, default: '' },
-  addressProv: { type: String, default: '' },
-  addressKab: { type: String, default: '' },
+  birthDate: { type: String, default: '' },
+  telepon: { type: String, default: '' },
+  province: { type: String, default: '' },
+  regency: { type: String, default: '' },
   work: { type: String, enum: ['PNS', 'Swasta', 'Lainnya'] },
   education: { type: String, enum: ['SLTA', 'D1/D2/D3', 'D4/S1', 'S2', 'S3'] },
   position: { type: String, default: '' },
-  field: { type: String, default: '' },
-  days: { type: [String], default: [] },
+  field: { type: [String], default: '' },
+  available: { type: [String], default: [] },
 });
 
 export default mongoose.model<IUser>('User', UserSchema);
