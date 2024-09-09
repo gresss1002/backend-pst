0b5dbc14ddce0bc981afcbc5ac0ad6129e8e2f61
gresss1002 <102232893+gresss1002@users.noreply.github.com>
2024-09-09 23:41:31 +0700

    change required reservasi

--- src/models/reservasiModels.ts
+++ src/models/reservasiModels.ts
@@ -18,13 +18,13 @@ export interface IReservasi extends Document {
 const ReservasiSchema = new Schema<IReservasi>({
     reservasiDate: { type: Date, required: true },
     time: { type: String, required: true },
-    idKonsultan: { type: String, required: true },
-    idKonsumen: { type: String, required: true },
+    idKonsultan: { type: String},
+    idKonsumen: { type: String},
     topic: { type: [String], required: true },
     method: { type: String, required: true },
-    queue: { type: Number, required: true },
-    status: { type: String, required: true },
-    link: { type: String, required: true },
+    queue: { type: Number},
+    status: { type: String},
+    link: { type: String },
     descriptionReservasi: { type: String },
 });
 
