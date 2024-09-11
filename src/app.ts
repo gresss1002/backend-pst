580cf72c7618e4f4e8b53880a31971fa773efa2c
gresss1002 <102232893+gresss1002@users.noreply.github.com>
2024-09-11 14:32:19 +0700

    timeout

--- src/app.ts
+++ src/app.ts
@@ -53,7 +53,7 @@ app.use('/complaint', complaintRoutes);
 
 mongoose
   .connect(MONGODB_URI, {
-    serverSelectionTimeoutMS: 500000, 
+    serverSelectionTimeoutMS: 1000000, 
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
